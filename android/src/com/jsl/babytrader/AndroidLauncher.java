171c1eca5349fea199d0aef12b824fca6ffb5868
crayna <craynafinal@gmail.com>
2017-06-03 13:17:16 -0700

    init commit

---
+++ android/src/com/jsl/babytrader/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jsl.babytrader;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jsl.babytrader.BabyTrader;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BabyTrader(), config);
+	}
+}

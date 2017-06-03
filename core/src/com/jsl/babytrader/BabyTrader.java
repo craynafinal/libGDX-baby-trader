029e298890b45c6889ced34524020a4c25614dbd
crayna <craynafinal@gmail.com>
2017-06-03 13:59:40 -0700

    game and screen tutorial

--- core/src/com/jsl/babytrader/BabyTrader.java
+++ core/src/com/jsl/babytrader/BabyTrader.java
@@ -1,33 +1,24 @@
 package com.jsl.babytrader;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.jsl.babytrader.Screens.PlayScreen;
 
-public class BabyTrader extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class BabyTrader extends Game {
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		setScreen(new PlayScreen(this));
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.render();
 	}
 }
